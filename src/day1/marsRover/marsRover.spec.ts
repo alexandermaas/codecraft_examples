71acd588038a52ecec5338dd0b7d1572b3b42143
Alexander Maas <alexander.maas@nl.abnamro.com>
2021-03-17 14:32:23 +0100

    Implement turn right command

--- src/day1/marsRover/marsRover.spec.ts
+++ src/day1/marsRover/marsRover.spec.ts
@@ -1,5 +1,5 @@
-import {MarsRover} from './marsRover';
-import {Heading} from './heading';
+import { MarsRover } from './marsRover';
+import { Heading } from './heading';
 
 describe('MarsRover', () => {
     describe('currentLocation', () => {
@@ -25,19 +25,55 @@ describe('MarsRover', () => {
     });
     describe('move forward', () => {
         [
-            {initialLocation: [5, 6], expectedLocation: [4,6], heading:Heading.West},
-            {initialLocation: [2, 3], expectedLocation: [1, 3], heading:Heading.West},
-            {initialLocation: [5, 6], expectedLocation: [6,6], heading:Heading.East},
-            {initialLocation: [2, 3], expectedLocation: [3, 3], heading:Heading.East},
-            {initialLocation: [5, 6], expectedLocation: [5,7], heading:Heading.North},
-            {initialLocation: [2, 3], expectedLocation: [2,4], heading:Heading.North},
-            {initialLocation: [5, 6], expectedLocation: [5,5], heading:Heading.South},
-            {initialLocation: [2, 3], expectedLocation: [2,2], heading:Heading.South},
-        ].forEach(({expectedLocation, heading, initialLocation}) => {
+            { initialLocation: [5, 6], expectedLocation: [4, 6], heading: Heading.West },
+            { initialLocation: [2, 3], expectedLocation: [1, 3], heading: Heading.West },
+            { initialLocation: [5, 6], expectedLocation: [6, 6], heading: Heading.East },
+            { initialLocation: [2, 3], expectedLocation: [3, 3], heading: Heading.East },
+            { initialLocation: [5, 6], expectedLocation: [5, 7], heading: Heading.North },
+            { initialLocation: [2, 3], expectedLocation: [2, 4], heading: Heading.North },
+            { initialLocation: [5, 6], expectedLocation: [5, 5], heading: Heading.South },
+            { initialLocation: [2, 3], expectedLocation: [2, 2], heading: Heading.South },
+        ].forEach(({ expectedLocation, heading, initialLocation }) => {
             it(`should move from location ${initialLocation} to location ${expectedLocation} if heading is '${heading}'`, () => {
                 const marsRover: MarsRover = new MarsRover(initialLocation, heading);
                 marsRover.command('F');
                 expect(marsRover.location).toEqual(expectedLocation);
+            });
+        });
+    });
+    describe('move backward', () => {
+        [
+            { initialLocation: [5, 6], expectedLocation: [6, 6], heading: Heading.West },
+            { initialLocation: [2, 3], expectedLocation: [3, 3], heading: Heading.West },
+            { initialLocation: [5, 6], expectedLocation: [4, 6], heading: Heading.East },
+            { initialLocation: [2, 3], expectedLocation: [1, 3], heading: Heading.East },
+            { initialLocation: [5, 6], expectedLocation: [5, 5], heading: Heading.North },
+            { initialLocation: [2, 3], expectedLocation: [2, 2], heading: Heading.North },
+            { initialLocation: [5, 6], expectedLocation: [5, 7], heading: Heading.South },
+            { initialLocation: [2, 3], expectedLocation: [2, 4], heading: Heading.South },
+        ].forEach(({ expectedLocation, heading, initialLocation }) => {
+            it(`should move from location ${initialLocation} to location ${expectedLocation} if heading is '${heading}'`, () => {
+                const marsRover: MarsRover = new MarsRover(initialLocation, heading);
+                marsRover.command('B');
+                expect(marsRover.location).toEqual(expectedLocation);
+            });
+        });
+    });
+    describe('turn', () => {
+        [
+            { initialHeading: Heading.North, expectedHeading: Heading.West, command: "L" },
+            { initialHeading: Heading.West, expectedHeading: Heading.South, command: "L" },
+            { initialHeading: Heading.South, expectedHeading: Heading.East, command: "L" },
+            { initialHeading: Heading.East, expectedHeading: Heading.North, command: "L" },
+            { initialHeading: Heading.East, expectedHeading: Heading.South, command: "R" },
+            { initialHeading: Heading.South, expectedHeading: Heading.West, command: "R" },
+            { initialHeading: Heading.West, expectedHeading: Heading.North, command: "R" },
+            { initialHeading: Heading.North, expectedHeading: Heading.East, command: "R" },
+        ].forEach(({ expectedHeading, initialHeading, command }) => {
+            it(`should move from '${initialHeading}' to '${expectedHeading}'`, () => {
+                const marsRover: MarsRover = new MarsRover([0, 0], initialHeading);
+                marsRover.command(command);
+                expect(marsRover.heading).toEqual(expectedHeading);
             });
         });
     });
--- src/day1/marsRover/marsRover.ts
+++ src/day1/marsRover/marsRover.ts
@@ -1,21 +1,55 @@
-import { Heading } from './heading';
+import {Heading} from './heading';
 
 export class MarsRover {
     constructor(public location: number[], public heading: Heading) {}
 
     command(command: string) {
+        let increment;
+        if (command == 'L') {
+            this.turnLeft();
+        }
+        else if(command == "R"){
+            if(this.heading == Heading.East){
+                this.heading = Heading.South
+            }
+            else if(this.heading == Heading.South){
+                this.heading = Heading.West
+            }
+            else if(this.heading == Heading.West){
+                this.heading = Heading.North
+            }
+            else if(this.heading == Heading.North){
+                this.heading = Heading.East
+            }
+        }
+        else if (command == 'F') {
+            increment = 1;
+            this.move(increment);
+        } else {
+            increment = -1;
+            this.move(increment);
+        }
+    }
+
+    private turnLeft() {
+        const directions = [Heading.North, Heading.East, Heading.South, Heading.West];
+        const directionIndex = directions.indexOf(this.heading);
+        this.heading = directions[(directionIndex - 1 + directions.length) % directions.length];
+    }
+
+    private move(increment: number) {
         switch (this.heading) {
             case Heading.West:
-                this.location = [this.location[0] - 1, this.location[1]];
+                this.location[0] -= increment;
                 break;
             case Heading.East:
-                this.location = [this.location[0] + 1, this.location[1]];
+                this.location[0] += increment;
                 break;
             case Heading.North:
-                this.location = [this.location[0], this.location[1] + 1];
+                this.location[1] += increment;
                 break;
             case Heading.South:
-                this.location = [this.location[0], this.location[1] - 1];
+                this.location[1] -= increment;
                 break;
         }
     }
