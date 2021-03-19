7e2db62317decfd1554740e950a6e96f02189f39
Alexander Maas <alexander.maas@nl.abnamro.com>
2021-03-19 09:43:05 +0100

    Refactor CD spec to use jest instead of jasmin+ts-mockito

--- wallaby.js
+++ wallaby.js
@@ -1,21 +1,26 @@
 module.exports = function (w) {
     return {
-        files: ['src/**/*.ts', '!src/**/*.spec.ts', 'src/**/*.json'],
+        files: ['src/**/*.ts', '!src/**/*.spec.ts', 'src/**/*.json', 'jest.json'],
 
         tests: ['src/**/*.spec.ts'],
 
         env: {
             type: 'node',
         },
-
+        setup: function (wallaby) {
+            var jestConfig = require('./jest.json');
+            /* for example:
+             * jestConfig.globals = { "__DEV__": true }; */
+            wallaby.testFramework.configure(jestConfig);
+        },
         compilers: {
             '**/*.ts?(x)': w.compilers.typeScript({
                 resolveJsonModule: true,
-                esModuleInterop: true,
+                esModuleInterop: true
             }),
         },
         // or any other supported testing framework:
         // https://wallabyjs.com/docs/integration/overview.html#supported-testing-frameworks
-        testFramework: 'jasmine',
+        testFramework: 'jest',
     };
 };
