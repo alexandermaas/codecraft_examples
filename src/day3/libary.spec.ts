ec09dfcde3b0ae8b34944dda1e8bb81bbd72d28c
Alexander Maas <alexander.maas@nl.abnamro.com>
2021-03-19 11:01:00 +0100

    Implement first example marketplace

--- src/day3/libary.spec.ts
+++ src/day3/libary.spec.ts
@@ -1,11 +1,23 @@
 import {Book} from "./book";
 import {Library} from "./library";
+import {mock} from "jest-mock-extended";
 
 const maxHavelaarBook = new Book("Max Havelaar", "Multatuli")
 const harryPotterBook1 = new Book("Harry Potter: Philosopher stone", "J.K. Rowling")
 const harryPotterBook2 = new Book("Harry Potter: Chamber of secrets", "J.K. Rowling")
 
-describe('Search cheapest price for book title', () => {
+class Listing {
+    constructor(bookStoreName: string, price: number, link: string) {
+    }
+}
+
+class MarketPlace {
+    getListing(book: Book) {
+        return new Listing("Amazon", 9.99, "http://amazon.com/multatuli/buy")
+    }
+}
+
+describe('Library', () => {
     describe('Book was found', () => {
         it('should return single book in case there is only one item in the library', () => {
             const library = new Library([maxHavelaarBook]);
@@ -18,17 +30,21 @@ describe('Search cheapest price for book title', () => {
             expect(book).toEqual(harryPotterBook2)
         });
     })
-    describe('Book was not found',() => {
+    describe('Book was not found', () => {
         it('should throw error in case library is empty', () => {
-            const library: Library= new Library([]);
-            expect(()=>{library.getBook(maxHavelaarBook.title, maxHavelaarBook.author)}).toThrowError("The book you are looking for ('Max Havelaar' by 'Multatuli') does not exist in our database")
+            const library: Library = new Library([]);
+            expect(() => {
+                library.getBook(maxHavelaarBook.title, maxHavelaarBook.author)
+            }).toThrowError("The book you are looking for ('Max Havelaar' by 'Multatuli') does not exist in our database")
         });
         it('should throw error in case book does not exist in library', () => {
             const library: Library = new Library([maxHavelaarBook]);
-            expect(()=>{library.getBook(harryPotterBook2.title, harryPotterBook2.author)}).toThrowError("The book you are looking for ('Harry Potter: Chamber of secrets' by 'J.K. Rowling') does not exist in our database")
+            expect(() => {
+                library.getBook(harryPotterBook2.title, harryPotterBook2.author)
+            }).toThrowError("The book you are looking for ('Harry Potter: Chamber of secrets' by 'J.K. Rowling') does not exist in our database")
         });
     })
-    describe('Multiple books same author',() => {
+    describe('Multiple books same author', () => {
         it('should return all books of the author in the library', () => {
             let library: Library = new Library([maxHavelaarBook, harryPotterBook1, harryPotterBook2]);
             const books = library.getBooks(harryPotterBook1.author);
@@ -38,6 +54,14 @@ describe('Search cheapest price for book title', () => {
             let library: Library = new Library([maxHavelaarBook, harryPotterBook1, harryPotterBook2]);
             const books = library.getBooks(maxHavelaarBook.author);
             expect(books).toEqual([maxHavelaarBook])
+        });
+    })
+    describe('One cheapest store', () => {
+        it('should return the cheapest store when we search for a book', () => {
+            const expectedListing = new Listing("Amazon", 9.99, "http://amazon.com/multatuli/buy")
+            const marketPlace = new MarketPlace()
+            const listing = marketPlace.getListing(maxHavelaarBook)
+            expect(listing).toEqual(expectedListing)
         });
     })
 })
